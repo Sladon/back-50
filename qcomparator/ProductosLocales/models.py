1df672ae84e29644b65bddcdf927959e1f50983b
Benjamin Figueroa Guzman <bfigueroa@miuandes.cl>
2023-10-10 20:07:18 -0300

    update model

--- qcomparator/ProductosLocales/models.py
+++ qcomparator/ProductosLocales/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Local(models.Model):
     nombre = models.CharField(max_length=100)
     ubicacion = models.CharField(max_length=100, null=True)
+    descripcion = models.TextField(default="")
     # Otros campos relacionados a los locales
 
 class Producto(models.Model):
@@ -10,3 +11,4 @@ class Producto(models.Model):
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=5, decimal_places=2)
     local = models.ForeignKey(Local, on_delete=models.CASCADE, null=True) # Establece la relación con el modelo Local
+    
