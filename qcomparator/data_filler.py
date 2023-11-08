ab372689c67dd6bbb02dfd02580a233d19396702
Stefano Romanini <spromanini@miuandes.cl>
2023-11-07 21:12:09 -0300

    started seed

--- qcomparator/data_filler.py
+++ qcomparator/data_filler.py
@@ -3,6 +3,7 @@ from django.db import models
 from faker import Faker
 import os
 import django
+import csv
 
 # Configurar la variable de entorno DJANGO_SETTINGS_MODULE
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qcomparator.settings")
@@ -23,43 +24,73 @@ def create_fake_users(num_users=10):
         user = User.objects.create_user(username=username, email=email, password=password)
 
 # Función para rellenar la base de datos con datos falsos
-def fill_database_with_fake_data(num_locales=10, num_productos_por_local=5, num_reviews_per_product=10):
-    fake = Faker()
+def fill_database_with_fake_data():
 
-    # Lista de etiquetas ficticias
-    fake_tags = ['Dulce', 'Salado', 'Picante', 'Vegetariano', 'Orgánico', 'Sin gluten']
+    # Create Locations
+    with open('./Locales.csv', 'r', newline = '') as csvfile:
 
-    # Rellena la tabla Local
-    for _ in range(num_locales):
-        nombre_local = fake.company()
-        ubicacion_local = fake.address()
-        descripcion_local = fake.text()
-        local = Local(nombre=nombre_local, ubicacion=ubicacion_local, descripcion=descripcion_local)
-        local.save()
+        for line in csvfile.readlines():
+            data = line.strip().split(',')
 
-        # Rellena la tabla Producto para cada Local
-        for _ in range(num_productos_por_local):
-            nombre_producto = fake.word()
-            descripcion_producto = fake.text()
-            precio_producto = round(random.uniform(200, 100000), 2)  # Precio aleatorio entre 1 y 100
+            nombre_local = data[0]
+            ubicacion_local = data[1]
+            descripcion_local = data[2]
+            local = Local(nombre=nombre_local, ubicacion=ubicacion_local, descripcion=descripcion_local)
+            local.save()
+    
+
+    # Create Products
+    with open('./Productos.csv', 'r', newline = '') as csvfile:
+
+        for line in csvfile.readlines():
+            data = line.strip().split(',')
+
+            nombre_producto = data[0]
+            descripcion_producto = data[1]
+            precio_producto = float(data[2])
+            local_id = int(data[3])
+            local = Local.objects.get(id = local_id)
             producto = Producto(nombre=nombre_producto, descripcion=descripcion_producto, precio=precio_producto, local=local)
             producto.save()
 
-            num_tags = random.randint(1, len(fake_tags))  # Número aleatorio de etiquetas a asignar
-            tags = fake.random_elements(elements=fake_tags, length=num_tags, unique=True)
+            tags = data[4].split(",")
             for tag_name in tags:
-                tag, created = Tag.objects.get_or_create(nombre=tag_name)
-                producto.tags.add(tag)  # Agrega la etiqueta al producto
+                tag, created = Tag.objects.get_or_create(nombre=tag_name) # If tag is found, then its returned. Else, it creates a new tag and returns the new created one
+                producto.tags.add(tag)
 
-            producto.save()  # Guarda el producto después de asociar las etiquetas
+            producto.save()
 
-            # Rellena la tabla Review para cada Producto
-            for _ in range(num_reviews_per_product):
-                user_id = random.randint(1, 10)  # Suponiendo que tienes 10 usuarios en la base de datos
-                comentario_review = fake.paragraph()
-                calificacion_review = random.randint(1, 5)  # Calificación aleatoria entre 1 y 5
-                review = Review(user_id=user_id, producto=producto, comentario=comentario_review, calificacion=calificacion_review)
-                review.save()
+    # Rellena la tabla Local
+    # for _ in range(num_locales):
+    #     nombre_local = fake.company()
+    #     ubicacion_local = fake.address()
+    #     descripcion_local = fake.text()
+    #     local = Local(nombre=nombre_local, ubicacion=ubicacion_local, descripcion=descripcion_local)
+    #     local.save()
+
+    #     # Rellena la tabla Producto para cada Local
+    #     for _ in range(num_productos_por_local):
+            # nombre_producto = fake.word()
+            # descripcion_producto = fake.text()
+            # precio_producto = round(random.uniform(200, 100000), 2)  # Precio aleatorio entre 1 y 100
+            # producto = Producto(nombre=nombre_producto, descripcion=descripcion_producto, precio=precio_producto, local=local)
+            # producto.save()
+
+            # num_tags = random.randint(1, len(fake_tags))  # Número aleatorio de etiquetas a asignar
+            # tags = fake.random_elements(elements=fake_tags, length=num_tags, unique=True)
+            # for tag_name in tags:
+            #     tag, created = Tag.objects.get_or_create(nombre=tag_name)
+            #     producto.tags.add(tag)  # Agrega la etiqueta al producto
+
+            # producto.save()  # Guarda el producto después de asociar las etiquetas
+
+    #         # Rellena la tabla Review para cada Producto
+    #         for _ in range(num_reviews_per_product):
+    #             user_id = random.randint(1, 10)  # Suponiendo que tienes 10 usuarios en la base de datos
+    #             comentario_review = fake.paragraph()
+    #             calificacion_review = random.randint(1, 5)  # Calificación aleatoria entre 1 y 5
+    #             review = Review(user_id=user_id, producto=producto, comentario=comentario_review, calificacion=calificacion_review)
+    #             review.save()
 
 
 if __name__ == "__main__":
